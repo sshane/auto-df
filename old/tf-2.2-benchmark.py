2bdb52869ab1a30f1188ab64ff7c0d274af3ccc2
Shane Smiskol <shane@smiskol.com>
2020-07-06 13:55:23 -0500

    update with an actual benchmark timer

--- old/tf-2.2-benchmark.py
+++ old/tf-2.2-benchmark.py
@@ -1,9 +1,10 @@
+import time
 import numpy as np
 from tensorflow.keras.layers import Dense
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.optimizers import Adam
 
-x_train = np.random.rand(10000, 50)
+x_train = np.random.rand(100000, 50)
 y_train = x_train.take(axis=1, indices=-1) * np.pi
 
 model = Sequential()
@@ -12,4 +13,6 @@ model.add(Dense(8, activation='relu'))
 model.add(Dense(1))
 
 model.compile(Adam(amsgrad=True), 'mse')
-model.fit(x_train, y_train, batch_size=32, validation_split=0.5, epochs=100)
+t = time.time()
+model.fit(x_train, y_train, batch_size=32, validation_split=0.2, epochs=20)
+print('time to completion: {} seconds'.format(round(time.time() - t, 4)))
