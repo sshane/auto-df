37ad5bc6da5d0bc23731eac1d3569d3e52157b63
Shane Smiskol <shane@smiskol.com>
2020-07-06 14:20:54 -0500

    add valid data

--- old/tf-2.2-benchmark.py
+++ old/tf-2.2-benchmark.py
@@ -4,9 +4,12 @@ import tensorflow as tf
 from tensorflow.keras.layers import Dense
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.optimizers import Adam
+from sklearn.model_selection import train_test_split
+from tensorflow.keras.mixed_precision import experimental as mixed_precision
 
 x_train = np.random.rand(100000, 50)
 y_train = x_train.take(axis=1, indices=-1) * np.pi
+x_train, x_test, y_train, y_test = train_test_split(x_train, y_train, test_size=0.2)
 
 model = Sequential()
 model.add(Dense(16, activation='relu', input_shape=x_train.shape[1:]))
@@ -16,5 +19,5 @@ model.add(Dense(1))
 model.compile(Adam(amsgrad=True), 'mse')
 print('TF VERSION: {}'.format(tf.__version__))
 t = time.time()
-model.fit(x_train, y_train, batch_size=32, validation_split=0.2, epochs=20)
+model.fit(x_train, y_train, batch_size=32, validation_data=(x_test, y_test), epochs=20)
 print('time to completion: {} seconds'.format(round(time.time() - t, 4)))
